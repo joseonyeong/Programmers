180dd41c979e53f7d520b30537c166ee545151f9
joseonyeong <whtjsdud44@gmail.com>
2025-03-20 12:41:49 +0900

    짝수와 홀 구하기
    lv.1

---
+++ lv1_Even_odd.py
@@ -0,0 +1,13 @@
+'''
+    정수 num이 짝수일 경우 "Even"을 반환하고 
+    홀수인 경우 "Odd"를 반환하는 함수
+'''
+
+def solution(num):
+    answer = ''
+    if num % 2 == 0:
+        answer = 'Even'
+    else:
+        answer = 'Odd'
+    return answer
+print(solution(4))
